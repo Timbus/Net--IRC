87011495058f9adbe6bd22d85bb9d34bc37eec16
Jarrod <gigantic.midget@gmail.com>
2010-08-03 03:41:45 +1000

    Refactor the old code; combine Dispatcher into Bot since there's no need to hide the methods from user modules now. Noticed something: Default behavious like nick tracking and etc now need to be modularized. Might think of a way to give users control over how they are included. For now, will just put the default methods into a class and push it into @modules during init.

--- Net/IRC/Bot.pm
+++ Net/IRC/Bot.pm
@@ -1,5 +1,36 @@
 use Net::IRC::Connection;
-use Net::IRC::Dispatcher;
+
+class User {
+	has $.fullnick;
+	has $.nick;
+	has $.host;
+	has $.ident;
+
+	multi method new ($s) {
+		~$s ~~ /^$<nick>=(.+?)\!$<ident>=(.+?)\@$<host>=(.+)$/
+			or fail('Type check failed for assignment');
+		
+		return $.bless(
+			*, 
+			fullnick => ~$s,
+			nick => ~$<nick>,
+			host => ~$<host>,
+			ident => ~$<ident>,
+		);
+	}
+	
+	multi method Str is export {
+		return $.nick;
+	}
+	
+	multi method match is export ($topic) {
+		return ($.nick ~~ $topic);
+	}
+}
+
+multi infix:<=> (User $u is rw, Str $s) {
+	$u .= new($s);	
+}
 
 class Net::IRC::Bot {
 	#has Net::IRC::Handler $manager;
@@ -208,5 +239,112 @@ class Net::IRC::Bot {
 	multi method ctcp_version($from, $channel) {
 		$.send_ctcp("VERSION Perl6bot 0.001a Probably *nix", $from);
 	}
+	
+	method run() {
+		$.connect();
+		loop {
+		
+			#XXX: Support for timed events?
+			
+			my $line = $connection.get 
+				or $.reconnect and next
+				or last;
+			
+			my $event = IRCEvent.parse($line)
+				or warn "Could not parse the following IRC event: $line";	
+			#---FOR DEBUGGING----
+			say ~$event;
+			#--------------------
+			
+			$.dispatch($event);
+			
+			CATCH {
+				#On the event of a dicconnect, we retry (if the bot is told to do so..)
+				if $! eq 'Server closed connection' {
+					$.reconnect;
+					next;
+				}
+				last;
+			}
+		}
+	}
+	
+	method dispatch(Match $event) {
+		#Dispatch to any raw irc_event handlers first
+		@modules>>.*"irc_{ lc $event<command> }"($event);
+
+		given ~$event<command> {
+			when "PRIVMSG" {
+				my $from = $.strip_nick(~$event<from>);
+				my $channel = ~$event<param>[0] ~~ /^\#.*/ || $from;
+				my $text = ~$event<text>;
+				
+				#Check to see if its a CTCP request.
+				if $text ~~ /^\c01 (.*) \c01$/ {
+					$text = ~$0;
+					say "Received CTCP $text from $from" ~ ( $channel eq $from ?? '.' !! " (to channel $channel)." );
+
+					if $text ~~ /^ ACTION\s (.*) $/ {
+						$.*emoted(~$0, ~$from, ~$channel);
+					}
+					else {
+						$text ~~ /^ (.+?) [\s(.*)]? $/;
+						if $1 {
+							@modules>>.*"ctcp_{ lc $0 }"(~$1, ~$from, ~$channel);
+						}
+						else {
+							@modules>>.*"ctcp_{ lc $0 }"(~$from, ~$channel);
+						}
+					}
+				}
+				
+				else {
+					@modules>>.*said(~$text, ~$from, ~$channel);
+				}
+			}
+	
+			when "NOTICE" {
+				my $from = $.strip_nick(~$event<from>);
+				my $channel = ~$event<param>[0] ~~ /^\#.*/ || $from;
+				@modules>>.*noticed(~$event<text>, ~$from, ~$channel);
+			}
+			
+			when "KICK" {
+				my $from = $.strip_nick(~$event<from>);
+				my $channel = ~$event<param>[0];
+				my $kicked = ~$event<param>[1];
+				@modules>>.*kicked($kicked, ~$event<text>, $from, $channel);
+			}
+			
+			when "JOIN" {
+				my $from = $.strip_nick(~$event<from>);
+				my $channel = ~$event<text>;
+				@modules>>.*joined($from, $channel);
+			}
+			
+			when "NICK" {
+				my $from = $.strip_nick(~$event<from>);
+				my $to = ~$event<text> // ~$event<param>[0];
+				@modules>>.*nickchange($from, $to);
+			}
+		
+			when "376"|"422" {
+				#End of motd / no motd. (Usually) The last thing a server sends the client on connect.
+				@modules>>.*connected;
+			}
+			
+			default {
+				@modules>>.*"{ lc $event<command> }"($event);
+			}
+		}
+		
+	}
 }
 
+grammar IRCEvent {
+	rule TOP {
+		| ^':'?$<command>=(P[I|O]NG) ':'?$<text>=(.+)?$
+		| ^':'?$<command>=(ERROR) ':'?$<text>=(.+)$
+		| ^':'?$<from>=<-space>+ $<command>=<-space>+[ <!before ':'>$<param>=<-space>+]*?[ ':'$<text>=(.+)]?<.ws>?$
+	}
+}#**
