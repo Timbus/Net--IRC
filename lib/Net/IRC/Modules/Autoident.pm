21fd9146fec4bb62c02acea06d1a0b05d83e10ab
Moritz Lenz <moritz@faui2k3.org>
2013-02-22 13:36:33 +0100

    Autoident: make password a public attribute
    otherwise you cannot initialize it from the constructor

--- lib/Net/IRC/Modules/Autoident.pm
+++ lib/Net/IRC/Modules/Autoident.pm
@@ -2,9 +2,9 @@ use v6;
 module Net::IRC::Modules::Autoident;
 
 class Net::IRC::Modules::Autoident {
-	has $password = die "Need to tell Autoident your password if you want it to work!";
+	has $.password = die "Need to tell Autoident your password if you want it to work!";
 	multi method connected($ev) {
 		say "Identifying with nickserv..";
-		$ev.conn.sendln("NS IDENTIFY $password");
+		$ev.conn.sendln("NS IDENTIFY $.password");
 	}
 }
