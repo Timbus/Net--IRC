acccb8fc4de7861ecedcc4074c039de9de023ade
Geoffrey Broadwell <gjb@google.com>
2014-01-19 22:02:34 -0800

    Switch SocketConnection to use supplies instead of channels for receiving data from the socket; include both recv (raw buffer data) and get (chomped lines) supply variants

--- lib/Net/IRC/SocketConnection.pm
+++ lib/Net/IRC/SocketConnection.pm
@@ -7,7 +7,10 @@ class Net::IRC::SocketConnection {
     has Net::IRC::Logger $.log;
     has IO::Socket       $.socket;
     has Channel          $.to-socket;
-    has Channel          $.from-socket;
+    has Supply           $.recv;
+    has Supply           $.get;
+    has Bool             $!recv-done;
+    has buf8             $!buffer = buf8.new;
 
     multi method new(:$host!, :$log = $*LOG, *%socket-options) {
         $log.info("Making socket connection to $host");
@@ -17,39 +20,70 @@ class Net::IRC::SocketConnection {
 
     multi method new(IO::Socket $socket, Net::IRC::Logger $log = $*LOG) {
         $log.debug('Building Net::IRC::SocketConnection');
-        my $to-socket   = Channel.new;
-        my $from-socket = Channel.new;
-        my $self = self.bless(:$log, :$socket, :$to-socket, :$from-socket);
+        my $to-socket = Channel.new;
+        my $recv = Supply.new;
+        my $self = self.bless(:$log, :$socket, :$to-socket, :$recv);
 
         $self!start-threads;
         $self;
     }
 
     method !start-threads() {
-        $.log.info('Starting channel -> socket thread');
+        # Line splitting assumptions:
+        # * Both lines and encoding groups can be split across chunks
+        # * The line separator can't be confused with a partial encoding group
+        # * Decoding is not horribly slow (so we can repeat it over incomplete
+        #   lines until we get the line separator without performance death)
+        # * Encoding isn't horribly slow either
+        $.log.info('Preparing socket supplies');
+        $!get := on -> $out {
+            $!recv => sub ($chunk) {
+                $!buffer ~= $chunk;
+                my $enc     := $!socket.encoding;
+                my $sep     := $!socket.input-line-separator;
+                my $decoded := $!buffer.decode($enc);
+                my @lines   := $decoded.split($sep);
+                if @lines > 1 {
+                    # Drop final partial line, and forward on the whole ones
+                    @lines.pop;
+                    $out.more($_) for @lines;
+
+                    # Remove already-forwarded portion of buffer
+                    my $forwarded := @lines.push('').join($sep).encode($enc);
+                    my $to-remove  = $forwarded.elems;
+                    $!buffer .= subbuf($to-remove);
+                }
+            }
+        };
+        $!recv.tap: {;}, done => { $!recv-done = True;
+                                   $*LOG.info('recv supply closed') };
+
         start {
+            $.log.info('Starting socket -> supplies thread');
+
+            loop {
+                my $chunk = $.socket.recv(:bin);
+                if $chunk.elems {
+                    $.log.debug("<-- $chunk.elems() bytes");
+                    $.recv.more($chunk);
+                }
+                else {
+                    $.log.info('Exiting socket -> supplies thread');
+                    $.recv.done;
+                    last;
+                }
+            }
+        }
+
+        start {
+            $.log.info('Starting channel -> socket thread');
+
             loop {
                 winner $.to-socket {
                     more * { $.log.debug("»»» $_.value()");
                              $.socket.write($_.key) }
                     done * { $.log.info('Exiting channel -> socket thread');
                              last }
-                }
-            }
-        }
-
-        $.log.info('Starting socket -> channel thread');
-        start {
-            loop {
-                my $chunk = $.socket.recv(:bin);
-                if $chunk.elems {
-                    $.log.debug("<-- $chunk.elems() bytes");
-                    $.from-socket.send($chunk);
-                }
-                else {
-                    $.log.info('Exiting socket -> channel thread');
-                    $.from-socket.close;
-                    last;
                 }
             }
         }
@@ -67,10 +101,14 @@ class Net::IRC::SocketConnection {
         $.to-socket.send: "$text\c13\c10".encode('utf8') => $scrubbed;
     }
 
-    method close(:$to, :$from) {
+    method close(:$to is copy, :$from is copy) {
         my $both = !($to ?^ $from);
-        $.to-socket.close   if $to   || $both;
-        $.from-socket.close if $from || $both;
-        $.socket.close if $.to-socket.closed && $.from-socket.closed;
+        $to    ||= $both;
+        $from  ||= $both;
+        $.log.info("Closing socket (to: {?$to}, from: {?$from})");
+
+        $.to-socket.close if $to;
+        $.recv.done       if $from && !$!recv-done;
+        $.socket.close    if $.to-socket.closed && $!recv-done;
     }
 }
